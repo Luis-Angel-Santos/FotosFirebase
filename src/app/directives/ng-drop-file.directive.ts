2baaa7cfde4ae50c965e4e6256d48a31041337bd
Luis Angel <santosangel748@gmail.com>
2020-11-18 21:59:00 -0600

    Haciendo las validaciones del archivo desde la directiva

--- src/app/directives/ng-drop-file.directive.ts
+++ src/app/directives/ng-drop-file.directive.ts
@@ -1,10 +1,12 @@
 import { Directive, EventEmitter, ElementRef, HostListener, Input, Output } from '@angular/core';
+import { FileItem } from '../models/file-item';
 
 @Directive({
   selector: '[appNgDropFile]'
 })
 export class NgDropFileDirective {
 
+  @Input() archivos: FileItem[] = [];
   @Output() mouseSobre: EventEmitter<boolean> = new EventEmitter();
 
   constructor() { }
@@ -16,6 +18,30 @@ export class NgDropFileDirective {
   @HostListener('dragleave', ['$event'])
   public onDragLeave(event: any){
     this.mouseSobre.emit(false);
+  }
+
+  private _archivoPuedeSerCargado(archivo: File): boolean{
+    if (!this._archivosYaFueDroppeado(archivo.name) && this._esImagen(archivo.type)) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+  private _prevenirDetener( event ){
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  private _archivosYaFueDroppeado(nombreArchivo: string): boolean{
+    for(const archivo of this.archivos){
+      if (archivo.nombreArchivo == nombreArchivo) {
+        console.log('El archivo '+nombreArchivo+' ya existe');
+        return true;
+      }
+    }
+    return false;
+  }
+  private _esImagen(tipoArchivo: string):boolean{
+    return (tipoArchivo === '' || tipoArchivo === undefined) ? false : tipoArchivo.startsWith('image');
   }
 
 }
