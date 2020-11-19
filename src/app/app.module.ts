d40127f104e1f80afccd34635e500cf6312a1a17
Luis Angel <santosangel748@gmail.com>
2020-11-18 21:05:20 -0600

    Configurando FireBase y creando el servicio de carga

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FotosComponent } from './components/fotos/fotos.component';
 import { CargaComponent } from './components/carga/carga.component';
+import { CargaImagenesService } from './services/carga-imagenes.service';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,9 +20,14 @@ import { CargaComponent } from './components/carga/carga.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [
+    CargaImagenesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,15 +2,18 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
-};
+import firebase from 'firebase';
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBm_wX9o-177RFvP05T16fqpgQqExG5TWw',
+    authDomain: 'fotos-ea81f.firebaseapp.com',
+    databaseURL: 'https://fotos-ea81f.firebaseio.com',
+    projectId: 'fotos-ea81f',
+    storageBucket: 'fotos-ea81f.appspot.com',
+    messagingSenderId: '245994926674',
+    appId: '1:245994926674:web:eb2e7e711f3a180d51a58b',
+    measurementId: 'G-Z2F21V0KX7'
+  }
+};
